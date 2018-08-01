e0c348f23b71feef8729fd17d2435362508d2903
Piotr Kudrel <piotr.kudrel@deneblab.com>
2018-08-01 21:45:29 +0200

    1.2018.306525
    Version: 1.2018.306525

--- publish.ps1
+++ publish.ps1
@@ -6,16 +6,17 @@ if(!$PSScriptRoot){
 
 
 $DATE = ((Get-Date).ToUniversalTime())
-$DATE_TEXT = ($DATE.ToString("yyyyMMddHHmm"))
+$DATE_TEXT = ($DATE.ToString("yyyy-MM-ddTHH:mm:ssZ"))
 $VERSION = ($DATE.ToString("yyyyMM.ddHHmm"))
 $DIFF = $DATE -  ((Get-Date -Year $DATE.Year -Month 1 -Day 1 -Hour 0 -Minute 0 -Second 0).ToUniversalTime())
 $SECONDS = [math]::Round($DIFF.TotalMinutes)
 $VERSION = "1.$($DATE.Year).$SECONDS"
+
+$DATA = [PSCustomObject]  @{ "Date" = $DATE_TEXT ; "SemVer" = "$semVer"}
+
 Write-Host "Version: $VERSION "
-
-
-$VERSION_FILE =  Join-Path $PSScriptRoot "VERSION"
-[System.IO.File]::WriteAllText($VERSION_FILE, $VERSION)
+$VERSION_FILE =  Join-Path $PSScriptRoot "version.json"
+[System.IO.File]::WriteAllText($VERSION_FILE, $DATA)
 
 $message = "Version: $VERSION"
 &git add .
